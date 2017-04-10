669f5d05c53105914d7ea245d11e64dde54678f4
colebob9 <colebob9@gmail.com>
2017-04-10 14:39:51 -0500

    Added GitHub release downloads

--- CPO-Organize.py
+++ CPO-Organize.py
@@ -1,5 +1,5 @@
 """
-CraftyPluginOrganizer v0.1
+CraftyPluginOrganizer v0.2
 Python
 
 Requires modules from pip:
@@ -12,6 +12,9 @@ TODO:
 GitHub latest release
 Test SpigotMC with different plugin.
 BukkitDev latest release
+Delete HTML after download is done.
+Server folder organizing
+
 
 """
 
@@ -19,9 +22,11 @@ BukkitDev latest release
 import subprocess
 import cfscrape
 from bs4 import BeautifulSoup
+import os, shutil
 
 # To find the latest download link from SpigotMC website, then download latest plugin with found link.
 def spigotmcLatestDownload(pluginName, url):
+    print("[DOWNLOAD] Downloading latest version of " + pluginName + " from SpigotMC.org.\n")
     pluginNameHtml = pluginName + ".html"
     pluginNameJar = pluginName + ".jar"
     spigotMCAddress = "https://www.spigotmc.org/"
@@ -50,22 +55,32 @@ def spigotmcLatestDownload(pluginName, url):
     
 # To download a plugin from SpigotMC.org. Needs specific download link.
 def spigotmcPluginDownload(pluginName, url):
+    print("[DOWNLOAD] Downloading " + pluginName + " from SpigotMC.\n")
     pluginName = pluginName + ".jar"
     cookie_arg, user_agent = cfscrape.get_cookie_string(url)
 
     subprocess.call(["curl", "-o", pluginName, "--cookie", cookie_arg, "-A", user_agent, url])
     
 def githubLatestRelease(pluginName, url):
-    print("test")
+    print("[DOWNLOAD] Downloading latest release of " + pluginName + " from GitHub")
+    cmd = ("""curl -s %s | grep browser_download_url | 
+grep '[.]jar' | head -n 1 | cut -d '"' -f 4""" % (url))
+    latest = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    output = latest.communicate()[0]
+    output = output.decode('utf8')
+    print(output)
+    
+    subprocess.call(["curl", "-o", pluginName + ".jar", "-L", output])
 
 # For any site that uses a permalink to download a specific or latest version. (Jenkins-LastSuccessfulBuild, Developer's Website, BukkitDev, etc.)
 def generalCurl(pluginName, url):
+    print("[DOWNLOAD] Downloading " + pluginName + " from URL: " + url)
     subprocess.call(["curl", "-o", pluginName + ".jar", url])
     
     
 # Put all download methods below here:
 
-generalCurl("ProtocolLib-Dev", "http://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/modules/ProtocolLib/target/ProtocolLib.jar")
-#githubLatestRelease("ProtocolLib" , "https://github.com/dmulloy2/ProtocolLib/releases")
-#spigotmcPluginDownloadDownload("ProtocolLib", "https://www.spigotmc.org/resources/protocollib.1997/download?version=131115")
-#spigotmcLatestDownload("ProtocolLib", "https://www.spigotmc.org/resources/protocollib.1997/")
+#generalCurl("ProtocolLib-Dev", "http://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/modules/ProtocolLib/target/ProtocolLib.jar")
+#githubLatestRelease("ProtocolLib-GitHub" , "https://api.github.com/repos/dmulloy2/ProtocolLib/releases")
+#spigotmcPluginDownload("ProtocolLib-spigotmcPluginDownload", "https://www.spigotmc.org/resources/protocollib.1997/download?version=131115")
+spigotmcLatestDownload("ProtocolLib", "https://www.spigotmc.org/resources/protocollib.1997/")
