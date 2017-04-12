10894849a3eff652cac028c9b1062842b624d6a5
colebob9 <colebob9@gmail.com>
2017-04-12 10:52:51 -0500

    Added configuration to specify file format

--- CPO-Organize.py
+++ CPO-Organize.py
@@ -1,5 +1,5 @@
 """
-CraftyPluginOrganizer v0.2
+CraftyPluginOrganizer v0.3
 Python
 
 Requires modules from pip:
@@ -18,11 +18,10 @@ any other CI
 
 TODO:
 
-Make overall download folder.
 Server folder organizing: date -> Server
-specify if it is a .zip or .jar
+    Use arguement in function call to specify which server directories to copy to.
 Check if link is an api or repo link.
-
+YAML config for configuration
 
 """
 
@@ -34,12 +33,17 @@ from bs4 import BeautifulSoup
 import requests
 
 
+if not os.path.exists("Download"):
+    os.mkdir("Download")
+    print("Made Download folder. All downloaded plugins will be stored here.")
+os.chdir("Download")
+    
+
 # To find the latest download link from SpigotMC website, then download latest plugin with found link.
 # Make sure this is used with a resource that has the download through SpigotMC, not a redirect to another website.
-def spigotmcLatestDownload(pluginName, url):
+def spigotmcLatestDownload(pluginName, url, fileFormat):
     print("[DOWNLOAD] Downloading latest version of " + pluginName + " from SpigotMC.org.\n")
     pluginNameHtml = pluginName + ".html"
-    pluginNameJar = pluginName + ".jar"
     spigotMCAddress = "https://www.spigotmc.org/"
     scraper = cfscrape.create_scraper()
     
@@ -63,21 +67,21 @@ def spigotmcLatestDownload(pluginName, url):
     # Download latest plugin.
 
     cookie_arg, user_agent = cfscrape.get_cookie_string(fullLatestDownload)
-    print("Downloading jar file: " + pluginNameJar)
-    subprocess.call(["curl", "-o", pluginNameJar, "--cookie", cookie_arg, "-A", user_agent, fullLatestDownload])
+    print("Downloading jar file: " + pluginName + fileFormat)
+    subprocess.call(["curl", "-o", pluginName + fileFormat, "--cookie", cookie_arg, "-A", user_agent, fullLatestDownload])
     # Sometimes fails with ProtocolLib, used too much?
     
 
     
 # To download a plugin from SpigotMC.org. Needs specific download link.
-def spigotmcPluginDownload(pluginName, url):
+def spigotmcPluginDownload(pluginName, url, fileFormat):
     print("[DOWNLOAD] Downloading " + pluginName + " from SpigotMC.\n")
-    pluginName = pluginName + ".jar"
+    pluginName = pluginName + fileFormat
     cookie_arg, user_agent = cfscrape.get_cookie_string(url)
 
     subprocess.call(["curl", "-o", pluginName, "--cookie", cookie_arg, "-A", user_agent, url])
     
-def githubLatestRelease(pluginName, url):
+def githubLatestRelease(pluginName, url, fileFormat):
     print("Link: " + url)
     if url.startswith('https://github.com'):
         print("URL is a normal release link. Converting to an API link...")
@@ -90,26 +94,35 @@ def githubLatestRelease(pluginName, url):
     
     
     print("[DOWNLOAD] Downloading latest release of " + pluginName + " from GitHub")
-    cmd = ("""curl -s %s | grep browser_download_url | grep '[.]jar' | head -n 1 | cut -d '"' -f 4""" % (url))
+    cmd = ("""curl -s %s | grep browser_download_url | grep '[.]%s' | head -n 1 | cut -d '"' -f 4""" % (url, fileFormat[1:]))
     latest = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     output = latest.communicate()[0]
     output = output.decode('utf8')
     print(output)
     
-    subprocess.call(["curl", "-o", pluginName + ".jar", "-L", output])
+    subprocess.call(["curl", "-o", pluginName + fileFormat, "-L", output])
 
 # For any site that uses a permalink to download a specific or latest version. (Jenkins-LastSuccessfulBuild, Developer's Website, BukkitDev, etc.)
-def generalCurl(pluginName, url):
+def generalCurl(pluginName, url, fileFormat):
     print("[DOWNLOAD] Downloading " + pluginName + " from URL: " + url)
-    subprocess.call(["curl", "-o", pluginName + ".jar", "-L", url])
+    subprocess.call(["curl", "-o", pluginName + fileFormat, "-L", url])
     
     
 # Put all download methods below here:
 
+
+githubLatestRelease("BlocksHub", "https://github.com/SBPrime/BlocksHub/releases", ".jar")
+spigotmcLatestDownload("BungeePortals", "https://www.spigotmc.org/resources/bungeeportals.19/", ".jar")
+generalCurl("ChestCommands", "https://dev.bukkit.org/projects/chest-commands/files/latest", ".jar")
+
+
+
+"""
 #generalCurl("ProtocolLib-Dev", "http://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/modules/ProtocolLib/target/ProtocolLib.jar")
-githubLatestRelease("ProtocolLib-GitHub" , "https://github.com/dmulloy2/ProtocolLib/releases")
+#githubLatestRelease("ProtocolLib-GitHub" , "https://github.com/dmulloy2/ProtocolLib/releases")
 #spigotmcPluginDownload("ProtocolLib-spigotmcPluginDownload", "https://www.spigotmc.org/resources/protocollib.1997/download?version=131115")
-#spigotmcLatestDownload("ProtocolLib", "https://www.spigotmc.org/resources/protocollib.1997/")
+spigotmcLatestDownload("ProtocolLib", "https://www.spigotmc.org/resources/protocollib.1997/")
 #spigotmcLatestDownload("RogueParkour", "https://www.spigotmc.org/resources/rogueparkour-random-generated-parkour.26563/")
 
 #generalCurl("Vault", "https://dev.bukkit.org/projects/vault/files/latest")
+"""
